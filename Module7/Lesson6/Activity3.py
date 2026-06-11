ba2704511ea51a95a87e0d90a50adb87f52beb29
pradumandhyani <pradumbd@gmail.com>
2026-06-11 11:23:50 +0530

    module 7 added

--- Module7/Lesson6/Activity3.py
+++ Module7/Lesson6/Activity3.py
@@ -4,7 +4,7 @@ s = turtle.Screen()
 colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow'] 
 s.bgcolor('black')
 t.speed('fastest') 
-t.hideturtle() 
+t.hideturtle()
 while True:
   for x in range(200): 
     t.pencolor(colors[x%len(colors)])
