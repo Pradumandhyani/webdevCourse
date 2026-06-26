6f37f6f27c07344d8f37f08e8b271002cca523eb
pradumandhyani <pradumbd@gmail.com>
2026-06-26 10:16:58 +0530

    assignment

---
+++ Module9/Assignment/Activity2.py
@@ -0,0 +1,20 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+computer = random.choice(choices)
+
+user = input("Enter Rock, Paper, or Scissors: ").lower()
+
+print("Computer chose:", computer)
+
+if user == computer:
+    print("It's a Tie!")
+elif (user == "rock" and computer == "scissors") or \
+     (user == "paper" and computer == "rock") or \
+     (user == "scissors" and computer == "paper"):
+    print("Congratulations! You Win!")
+elif user in choices:
+    print("Computer Wins!")
+else:
+    print("Invalid Choice!")
