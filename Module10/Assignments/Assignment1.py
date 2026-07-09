72eddaa59c39ebca8b9d16bcc3e2e1f3d8c32636
pradumandhyani <pradumbd@gmail.com>
2026-07-09 12:27:21 +0530

    module 10 added

---
+++ Module10/Assignments/Assignment1.py
@@ -0,0 +1,12 @@
+# Program to convert Binary to Decimal manually
+
+binary = input("Enter a binary number: ")
+
+decimal = 0
+power = 0
+
+for digit in binary[::-1]:
+    decimal += int(digit) * (2 ** power)
+    power += 1
+
+print("Decimal number is:", decimal)
